4890f6018fcc1b5f031a37ce37b0415a3f1a9a99
Diego <diegolinaresmojacar@gmail.com>
2025-03-20 20:11:48 +0100

    adicion de dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: "static", // Important for Netlify static hosting
+  outDir: "dist",
 });
